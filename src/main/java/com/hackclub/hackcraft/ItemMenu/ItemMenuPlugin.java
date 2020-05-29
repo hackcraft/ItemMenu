77a3a9339de5670cf6b0793d6d9d79f5ad2e0e5a
nice6599 <ncs72mc@gmail.com>
2020-05-29 17:22:21 -0400

    create item menu plugin

--- src/main/java/com/hackclub/hackcraft/ItemMenu/ItemMenuPlugin.java
+++ src/main/java/com/hackclub/hackcraft/ItemMenu/ItemMenuPlugin.java
@@ -1,19 +1,43 @@
 package com.hackclub.hackcraft.ItemMenu;
 
 
+import com.hackclub.hackcraft.ItemMenu.commands.ItemMenuCommand;
+import com.hackclub.hackcraft.ItemMenu.listeners.ItemActionListener;
+import com.hackclub.hackcraft.ItemMenu.listeners.PlayerListener;
+import com.hackclub.hackcraft.ItemMenu.objects.ItemMenu;
+import com.hackclub.hackcraft.ItemMenu.utils.ItemMenuUtil;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ItemMenuPlugin extends JavaPlugin {
 
 
+    public ItemMenuUtil ItemMenuUtil;
+
     @Override
     public void onEnable() {
-        getLogger().info("ItemMenus activated!");
+        getLogger().info("ItemMenu activated!");
+
+        // register serialization
+        ConfigurationSerialization.registerClass(ItemMenu.class);
+
+        // initialize custom objects
+        ItemMenuUtil = new ItemMenuUtil(this);
+        ItemMenuUtil.loadItemMenus();
+
+
+        // register commands
+        this.getCommand("menu").setExecutor(new ItemMenuCommand(this));
+
+        getServer().getPluginManager().registerEvents(new ItemActionListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+
+
 
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("ItemMenus deactivated!");
+        getLogger().info("ItemMenu deactivated!");
     }
 }
