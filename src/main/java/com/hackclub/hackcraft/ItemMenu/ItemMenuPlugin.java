5f0dab77234ffac688760001bff8c1f9f5a4cf65
nice6599 <ncs72mc@gmail.com>
2020-05-30 19:12:54 -0400

    add method to get menu util

--- src/main/java/com/hackclub/hackcraft/ItemMenu/ItemMenuPlugin.java
+++ src/main/java/com/hackclub/hackcraft/ItemMenu/ItemMenuPlugin.java
@@ -1,43 +1,42 @@
 package com.hackclub.hackcraft.ItemMenu;
 
-
 import com.hackclub.hackcraft.ItemMenu.commands.ItemMenuCommand;
 import com.hackclub.hackcraft.ItemMenu.listeners.ItemActionListener;
 import com.hackclub.hackcraft.ItemMenu.listeners.PlayerListener;
 import com.hackclub.hackcraft.ItemMenu.objects.ItemMenu;
 import com.hackclub.hackcraft.ItemMenu.utils.ItemMenuUtil;
+
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ItemMenuPlugin extends JavaPlugin {
 
+  public ItemMenuUtil ItemMenuUtil;
 
-    public ItemMenuUtil ItemMenuUtil;
+  @Override
+  public void onEnable() {
+    getLogger().info("ItemMenu activated!");
 
-    @Override
-    public void onEnable() {
-        getLogger().info("ItemMenu activated!");
+    // register serialization
+    ConfigurationSerialization.registerClass(ItemMenu.class);
 
-        // register serialization
-        ConfigurationSerialization.registerClass(ItemMenu.class);
+    // initialize custom objects
+    ItemMenuUtil = new ItemMenuUtil(this);
+    ItemMenuUtil.loadItemMenus();
 
-        // initialize custom objects
-        ItemMenuUtil = new ItemMenuUtil(this);
-        ItemMenuUtil.loadItemMenus();
+    // register commands
+    this.getCommand("menu").setExecutor(new ItemMenuCommand(this));
 
+    getServer().getPluginManager().registerEvents(new ItemActionListener(this), this);
+    getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+  }
 
-        // register commands
-        this.getCommand("menu").setExecutor(new ItemMenuCommand(this));
+  @Override
+  public void onDisable() {
+    getLogger().info("ItemMenu deactivated!");
+  }
 
-        getServer().getPluginManager().registerEvents(new ItemActionListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
-
-
-
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("ItemMenu deactivated!");
-    }
+  public ItemMenuUtil getMenuUtil() {
+    return ItemMenuUtil;
+  }
 }
